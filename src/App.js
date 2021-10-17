b82438298da955aee09437ed16fc0e7f8236d302
Adam <259355@student.pwr.edu.pl>
2021-10-17 20:53:19 +0200

    navbar changed and picture added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import picture from '../src/Pictures/picture_of_me.jpg';
 import './App.css';
 
 function App() {
@@ -6,15 +7,17 @@ function App() {
     <div className="App">
       <ul className="navbar">
         <li id="begin1"> Modlich Adam </li>
-        <div>
-          <li id="benefits1"> Benefits</li>
-          <li id="projects1"> Projects</li>
-          <li id="process1"> Process</li>
-          <li id="faq1"> FAQ</li>
-        </div>
-        <li id="contact1"> Contact me</li>
+        <li id="benefits1"> Benefits</li>
+        <li id="projects1"> Projects</li>
+        <li id="process1"> Process</li>
+        <li id="faq1"> FAQ</li>
+        <li id="contact1"> Contact me </li>
       </ul>
-      
+      <header>
+        <img src={picture} alt=""/>
+        <h2>MODLICH ADAM</h2>
+        <h1>WEBSITE DEVELOPER</h1>
+      </header>
     </div>
   );
 }
