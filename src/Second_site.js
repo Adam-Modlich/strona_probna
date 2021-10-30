7df3c637c22ceeec29e3faae2505f8b37004b983
Adam <259355@student.pwr.edu.pl>
2021-10-30 23:09:39 +0200

    working on images

--- src/Second_site.js
+++ src/Second_site.js
@@ -1,4 +1,9 @@
 import './second_site.scss';
+import rzym from '../src/Pictures/rzym.jpg';
+import portugalia from '../src/Pictures/portugalia.jpg';
+import grecja from '../src/Pictures/Grecja1.jpg';
+import paryz2 from '../src/Pictures/paryz2.jpg';
+
 
 
 function Second_site() {
@@ -11,8 +16,20 @@ function Second_site() {
             <h3><span>Website Developer </span><span id="content1">who will do it right</span></h3>
             <p>Cillum proident incididunt laborum irure eiusmod cillum mollit ipsum. Mollit ad eiusmod ea Lorem labore elit reprehenderit. Mollit quis tempor voluptate nulla. Nisi nostrud dolore minim incididunt qui magna laborum. Esse ut commodo eu veniam do sit. Ex cupidatat irure et sit dolor aliquip tempor ut cupidatat ullamco. Lorem anim labore fugiat minim fugiat nisi dolor ea.</p>
         </div>
-        <div id="box1"><h3>Trips</h3></div>
-        <div id="box2"><h3>Coding</h3></div>
+        <div id="box1"><h3>Trips</h3>
+            <p>
+                Moją główną pasją są podróże. Już będąc małym dzieckiem chciałem zwiedzić cały świat, poznać wszystkie kultury, dania oraz ludzi.
+            </p>  
+                          
+            <img id="img1" src={rzym}></img>
+            <img id="img2" src={paryz2}></img>
+
+        </div>
+        <div id="box2"><h3>Coding</h3>
+            <p>
+                
+            </p>
+        </div>
         <div id="box3"><h3>Cooking</h3></div>
         <div id="box4"><h3>Sport</h3></div>
         </div>
